8ba307905b81eb5e191e552d1392f65a2548a73a
Kabhilan <kabhilan05@gmail.com>
2024-05-02 18:09:19 +0530

    adding css in workarea

--- live-chat-app-client/src/Components/MainContainer.js
+++ live-chat-app-client/src/Components/MainContainer.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import './myStyles.css'
+import Sidebar from './Sidebar'
+import WorkArea from './WorkArea'
 
 function MainContainer() {
   return (
-    <div>MainContainer</div>
+    <div className='main-container'>
+        <Sidebar/>
+        <WorkArea/>
+    </div>
   )
 }
 
