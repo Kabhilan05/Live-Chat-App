b43879e8c1feee610de4c0acd87ff90da2c06dba
Kabhilan <kabhilan05@gmail.com>
2024-05-02 15:50:56 +0530

    Client side setup

---
+++ live-chat-app-client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import MainContainer from './Components/MainContainer';
+
+function App() {
+  return (
+    <div className="App">
+     APP
+     <MainContainer/>
+    </div>
+  );
+}
+
+export default App;
